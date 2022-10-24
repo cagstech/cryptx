60702536e2f8aac1699b0eb54bdcacd428003691
Anthony Cagliano <acagliano97@gmail.com>
2022-10-24 11:12:27 -0400

    fix some things in header

--- encodex/encodex.h
+++ encodex/encodex.h
@@ -23,16 +23,17 @@
  * See asn1_decode()
  */
 typedef struct _asn1_obj_t {
-	uint8_t tag;
-	uint8_t f_class;
-	bool f_constr;
-	size_t len;
-	uint8_t *data;
+	uint8_t tag;			/**< Defines the ASN.1 element tag (low 5 bits of the id) */
+	uint8_t f_class;		/**< Defines the ASN.1 class (high 2 bits of the id) */
+	bool f_form;			/**< Defines the ASN.1 construction scheme (bit 5 of the id) */
+	size_t len;				/**< Defines the length of the data portion of the element */
+	uint8_t *data;			/**< Defines a pointer to the data portion of the element */
 } asn1_obj_t;
 
 /*********************************
- * @enum ASN1_TYPES
+ * @enum ASN1\_TYPES
  * Defines tag identifiers for ASN.1 encoding
+ * See @b asn1_obj_t.tag
  */
 enum ASN1_TYPES {
 	ANS1_RESVD = 0,
@@ -64,6 +65,28 @@ enum ASN1_TYPES {
 	ASN1_UNIVERSALSTRING,
 	ASN1_CHARSTRING,
 	ASN1_BMPSTRING
+};
+
+/**********************************
+ * @enum ASN1\_CLASSES
+ * Defines class identifiers for ASN.1 encoding.
+ * See @b asn1_obj_t.f_class
+ */
+enum ASN1_CLASSES {
+	ASN1_UNIVERSAL,
+	ASN1_APPLICATION,
+	ASN1_CONTEXTSPEC,
+	ASN1_PRIVATE
+};
+
+/********************
+ * @enum ASN1\_FORMS
+ * Defines form identifiers for ASN.1 encoding.
+ * See @b asn1_obj_t.f_form
+ */
+enum ASN1_FORMS {
+	ASN1_PRIMATIVE,
+	ASN1_CONSTRUCTED
 };
 
 /****************************************************************
