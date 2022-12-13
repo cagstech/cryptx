f6506f2ff4677563dae7b912d351b0bb39b33603
Anthony Cagliano <acagliano97@gmail.com>
2022-12-13 16:13:03 -0500

    forgot a few changes in the docs

--- encrypt/examples/ecdh_demo/src/main.c
+++ encrypt/examples/ecdh_demo/src/main.c
@@ -22,7 +22,7 @@ void hexdump(uint8_t *addr, size_t len, uint8_t *label){
 	else sprintf(CEMU_CONSOLE, "\n");
 	for(size_t rem_len = len, ct=1; rem_len>0; rem_len--, addr++, ct++){
 		sprintf(CEMU_CONSOLE, "%02X ", *addr);
-		if(!(ct%AES_BLOCKSIZE)) sprintf(CEMU_CONSOLE, "\n");
+		if(!(ct%CRYPTX_AES_BLOCK_SIZE)) sprintf(CEMU_CONSOLE, "\n");
 	}
 	sprintf(CEMU_CONSOLE, "\n");
 }
@@ -30,19 +30,18 @@ void hexdump(uint8_t *addr, size_t len, uint8_t *label){
 
 int main(void)
 {
-	uint8_t secret1[ECDH_PUBKEY_SIZE];		// privkey1 * pubkey2
-	uint8_t secret2[ECDH_PUBKEY_SIZE];		// privkey2 * pubkey1
+	uint8_t secret1[CRYPTX_ECDH_PUBKEY_SIZE];		// privkey1 * pubkey2
+	uint8_t secret2[CRYPTX_ECDH_PUBKEY_SIZE];		// privkey2 * pubkey1
 	
-	ecdh_ctx test1;
-	ecdh_ctx test2;
+	struct cryptx_ecdh_ctx test1, test2;
 	ecdh_error_t err;
 	
 	// Always check for false return value from csrand_init()
-	if(!csrand_init(SAMPLING_FAST)) return 1;
+	if(!cryptx_csrand_init(SAMPLING_FAST)) return 1;
 	
 	sprintf(CEMU_CONSOLE, "\n\n----------------------------------\nElliptic Curve Diffie-Hellman Demo\n");
 	
-	err = ecdh_keygen(&test1, csrand_fill);
+	err = cryptx_ecdh_init(&test1);
 	sprintf(CEMU_CONSOLE, "gen of keypair 1 complete, exit code:%u\n", err);
 	if(!err){
 		hexdump(test1.privkey, sizeof test1.privkey, "---keypair 1 private---");
@@ -50,19 +49,19 @@ int main(void)
 	}
 	
 	
-	err = ecdh_keygen(&test2, csrand_fill);
+	err = cryptx_ecdh_init(&test2);
 	sprintf(CEMU_CONSOLE, "gen of keypair 2 complete, exit code:%u\n", err);
 	if(!err){
 		hexdump(test2.privkey, sizeof test2.privkey, "---keypair 2 private---");
 		hexdump(test2.pubkey, sizeof test2.pubkey, "---keypair 2 public---");
 	}
 	
-	err = ecdh_secret(&test1, test2.pubkey, secret1);
+	err = cryptx_ecdh_secret(&test1, test2.pubkey, secret1);
 	sprintf(CEMU_CONSOLE, "gen of secret1 complete, exit code:%u\n", err);
 	if(!err)
 		hexdump(secret1, sizeof secret1, "---Secret 1 = Privkey1 * Pubkey2 * Cofactor---");
 	
-	err = ecdh_secret(&test2, test1.pubkey, secret2);
+	err = cryptx_ecdh_secret(&test2, test1.pubkey, secret2);
 	sprintf(CEMU_CONSOLE, "gen of secret2 complete, exit code:%u\n", err);
 	if(!err)
 		hexdump(secret2, sizeof secret2, "---Secret 2 = Privkey2 * Pubkey1 * Cofactor---");
