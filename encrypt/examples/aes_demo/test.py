2e19ed5c4f0659d6647910b5567c17bc4c8f25b4
Anthony Cagliano <acagliano97@gmail.com>
2023-04-15 14:09:25 -0400

    change a word

--- encrypt/examples/aes_demo/test.py
+++ encrypt/examples/aes_demo/test.py
@@ -15,6 +15,7 @@ key = b"\xEE\x89\x19\xC3\x8D\x53\x7A\xD6\x04\x19\x9E\x77\x0B\xE0\xE0\x4C\x4C\x70
 iv = b"\x79\xA6\xDE\xDF\xF0\xA2\x7C\x7F\xEE\x0B\x8E\xF5\x12\x63\xA4\x8A"
 pt1 = b"The lazy fox jumped over the dog!"
 pt2 = b"The lazier fox fell down!"
+aad = "Some random header"
 
 if MODE==CipherMode.CBC:
     cipher_encrypt = AES.new(key, AES.MODE_CBC, iv=iv)
@@ -24,6 +25,7 @@ elif MODE==CipherMode.CTR:
     cipher_decrypt = AES.new(key, AES.MODE_CTR, nonce=iv[:8], initial_value=iv[8:])
     
 # encrypt both messages
+cipher_encrypt.update(aad)
 ct1 = cipher_encrypt.encrypt(pt1)
 ct2 = cipher_encrypt.encrypt(pt2)
 print(f"encrypted message 1\n{ct1.hex()}\n")
