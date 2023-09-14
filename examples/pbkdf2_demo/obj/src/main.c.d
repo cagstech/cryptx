f32c8132c1dc6b198e65e4550891b705bf449d64
Anthony Cagliano <acagliano97@gmail.com>
2023-09-13 20:11:12 -0400

    remove obj and bin

--- examples/pbkdf2_demo/obj/src/main.c.d
+++
@@ -1,9 +0,0 @@
-obj/src/main.c.bc: src/main.c /Users/acagliano/CEdev/include/stdint.h \
-  /Users/acagliano/CEdev/include/cdefs.h \
-  /Users/acagliano/CEdev/include/stdlib.h \
-  /Users/acagliano/CEdev/include/stdbool.h \
-  /Users/acagliano/CEdev/include/string.h \
-  /Users/acagliano/CEdev/include/stdio.h \
-  /Users/acagliano/CEdev/include/stdarg.h \
-  /Users/acagliano/CEdev/include/cryptx.h \
-  /Users/acagliano/CEdev/include/stddef.h
