30841b462f4ce3b66c597de135f03617b5983fa3
Anthony Cagliano <acagliano97@gmail.com>
2023-09-26 09:56:46 -0400

    add pkcs8 private key

--- src/pkcs8.c
+++ src/pkcs8.c
@@ -81,6 +81,81 @@ pkcs_error_t cryptx_pkcs8_import_publickey(struct cryptx_pkcs8_publickey *pkcs,
   return PKCS_OK;
 }
 
+pkcs_error_t cryptx_pkcs8_import_privatekey(struct cryptx_pkcs8_privatekey *pkcs,
+                                           void *data, size_t len, const char *passphrase){
+  if((data == NULL) ||
+     (len == 0) ||
+     (pkcs == NULL)) return PKCS_INVALID_ARG;
+  if(sizeof(pkcs) < len) return PKCS_INVALID_STRUCT;
+  
+  // verify that PEM banner matches expectation
+  bool encrypted;
+  char *test_str1 = "-----BEGIN PRIVATE KEY-----";
+  char *test_str2 = "-----BEGIN ENCRYPTED PRIVATE KEY-----";
+  if(strncmp(data, test_str1, strlen(test_str1))==0)
+    encrypted = false;
+  else if(strncmp(data, test_str2, strlen(test_str2))==0)
+    encrypted = true;
+  else return PKCS_INVALID_DATA;
+  
+  // strip PEM headers
+  uint8_t *pem_start = data;
+  while(*pem_start++ != '\n');
+  uint8_t *pem_end = data + len;
+  while(*pem_end-- != '\n');
+  size_t pem_size = pem_end - pem_start;
+  
+  // decode PEM/Base64
+  size_t der_size = pem_size * 8 / 6;
+  uint8_t der_buf[der_size];
+  cryptx_base64_decode(der_buf, pem_start, pem_size);
+  
+  // declare variables to hold ASN.1 metadata
+  uint8_t tag;
+  uint8_t *unwrap1, *unwrap2, *object;
+  size_t unwrap1_len, unwrap2_len, object_len;
+  asn1_error_t err = ASN1_OK;
+  size_t w_offset = 0;    // location in struct to copy decoded data
+  
+  // decode containing SEQUENCE
+  err = cryptx_asn1_decode(der_buf, der_size, 0, &tag, &unwrap1, &unwrap1_len);
+  if(err || (cryptx_asn1_gettag(tag) != ASN1_SEQUENCE))
+    return PKCS_INVALID_DATA;
+  
+  // decode algorithm ID and parameters
+  for(int i=0; i<=PKCS_PARAMS; i++){
+    err = cryptx_asn1_decode(unwrap1, unwrap1_len, i, &tag, &object_len, &object);
+    if(err || (cryptx_asn1_gettag(tag) != _pkcs_validator_keyinfo[i]))
+      return PKCS_INVALID_DATA;
+    pkcs->info[i].data = &pkcs->raw_data[w_offset];
+    pkcs->info[i].len = object_len;
+    memcpy(&pkcs->raw_data[w_offset], object, object_len);
+    w_offset += object_len;
+  }
+  
+  // decrypt key
+  if(encrypted == true){
+    
+  }
+
+  // decode public key PKCS#1 structure
+  err = cryptx_asn1_decode(unwrap1, unwrap1_len, PKCS_KEYINFO, &tag, &unwrap2_len, &unwrap2);
+  if(err || (cryptx_asn1_gettag(tag) != ASN1_SEQUENCE))
+    return PKCS_INVALID_DATA;
+  
+  for(int i=0; i<=PKCS_PRIVKEY_PRIMEINFO; i++){
+    err = cryptx_asn1_decode(unwrap2, unwrap2_len, i, &tag, &object_len, &object);
+    if(err || (cryptx_asn1_gettag(tag) != _pkcs_validator_privatekey[i]))
+      return PKCS_INVALID_DATA;
+    pkcs->privatekey.info[i].data = &pkcs->raw_data[w_offset];
+    pkcs->privatekey.info[i].len = object_len;
+    memcpy(&pkcs->raw_data[w_offset], object, object_len);
+    w_offset += object_len;
+  }
+  return PKCS_OK;
+}
+
+
 
 void cryptx_pkcs8_destroy(struct cryptx_pkcs8_key *pkcs){
   memset(pkcs, 0, sizeof(pkcs));
